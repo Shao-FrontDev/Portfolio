ccc21cb7e104ba986bc310d4aec26ae84fdd5d44
Richard <shaodeyu@foxmail.com>
2021-06-25 23:02:51 +0800

    调整

--- src/components/SocialMedia.vue
+++ src/components/SocialMedia.vue
@@ -23,54 +23,8 @@
   <!-- Followers tab -->
   <div class="pl-2 pb-2 border-custom-gray">
     <ul class="flex">
-      <a href="https://twitter.com/iamlardBendtner" target="_blank">
-        <li class="text-center cursor-pointer">
-          <span class="text-center">
-            <svg
-              class="text-main-blue h-6 pl-3 pr-3"
-              xmlns="http://www.w3.org/2000/svg"
-              xmlns:xlink="http://www.w3.org/1999/xlink"
-              viewBox="0 0 24 24"
-            >
-              <path
-                d="M22 4.01c-1 .49-1.98.689-3 .99c-1.121-1.265-2.783-1.335-4.38-.737S11.977 6.323 12 8v1c-3.245.083-6.135-1.395-8-4c0 0-4.182 7.433 4 11c-1.872 1.247-3.739 2.088-6 2c3.308 1.803 6.913 2.423 10.034 1.517c3.58-1.04 6.522-3.723 7.651-7.742a13.84 13.84 0 0 0 .497-3.753C20.18 7.773 21.692 5.25 22 4.009z"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              ></path>
-            </svg>
-          </span>
-        </li>
-      </a>
-      <a href="https://www.linkedin.com/in/darshanaswath/" target="_blank">
-        <li class="text-center cursor-pointer">
-          <span class="text-center">
-            <svg
-              class="text-main-blue h-6 pl-3 pr-3"
-              xmlns="http://www.w3.org/2000/svg"
-              xmlns:xlink="http://www.w3.org/1999/xlink"
-              viewBox="0 0 24 24"
-            >
-              <g
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              >
-                <rect x="4" y="4" width="16" height="16" rx="2"></rect>
-                <path d="M8 11v5"></path>
-                <path d="M8 8v.01"></path>
-                <path d="M12 16v-5"></path>
-                <path d="M16 16v-3a2 2 0 0 0-4 0"></path>
-              </g>
-            </svg>
-          </span>
-        </li>
-      </a>
-      <a href="https://github.com/xanf-code" target="_blank">
+    
+      <a href="https://github.com/Shao-FrontDev" target="_blank">
         <li class="text-center cursor-pointer">
           <span class="text-center">
             <svg
